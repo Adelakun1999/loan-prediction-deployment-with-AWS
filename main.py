4d7024b7974fd0cad3e08d056439b31c1699a127
Adelakun Yusuf Adekunle <99600634+Adelakun1999@users.noreply.github.com>
2024-11-20 17:38:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class LoanPred(BaseModel):
 
 @app.get('/')
 def index():
-    return {'Message' : 'Welcome to Loan Approval Prediction application'}
+    return {'Message' : 'Welcome to Loan Approval Prediction App 🤖'}
 
 #defining the function which will make the prediction using the data which the user inputs
 @app.post('/predict')
